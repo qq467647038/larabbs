0035b8394629f31325b670dbd3b52f4e05e42622
1 <1@qq.com>
2019-09-19 18:14:03 +0800

    修复 XSS 注入漏洞

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Models\Topic;
+use App\Observers\TopicObserver;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // 注册观察器
+        Topic::observe(TopicObserver::class);
     }
 }
