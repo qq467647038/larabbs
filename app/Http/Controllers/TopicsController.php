c55a6e9cad1991d539580b011e7fbd5aacbdb3a1
1 <1@qq.com>
2019-09-20 03:23:49 +0800

    翻译 URL 标示

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -47,6 +47,11 @@ class TopicsController extends Controller
 
     public function show(Topic $topic)
     {
+        // URL 矫正
+        if ( ! empty($topic->slug) && $topic->slug != $request->slug) {
+            return redirect($topic->link(), 301);
+        }
+		
         return view('topics.show', compact('topic'));
     }
 
@@ -62,7 +67,7 @@ class TopicsController extends Controller
         $topic->user_id = Auth::id();
         $topic->save();
 		
-		return redirect()->route('topics.show', $topic->id)->with('success', '帖子创建成功！');
+		return redirect()->to($topic->link())->with('success', '成功创建话题！');
 	}
 
 	public function edit(Topic $topic)
@@ -77,7 +82,7 @@ class TopicsController extends Controller
 		$this->authorize('update', $topic);
 		$topic->update($request->all());
 
-		return redirect()->route('topics.show', $topic->id)->with('message', 'Updated successfully.');
+		return redirect()->to($topic->link())->with('message', 'Updated successfully.');
 	}
 
 	public function destroy(Topic $topic)
