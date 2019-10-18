9d14977d88e718525a10ddf5f1c18081b1c8951d
1 <1@qq.com>
2019-10-18 12:25:11 +0800

    replies index

--- app/Http/Controllers/Api/RepliesController.php
+++ app/Http/Controllers/Api/RepliesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Models\Topic;
 use App\Models\Reply;
+use App\Models\User;
 use App\Transformers\ReplyTransformer;
 use App\Http\Requests\Api\ReplyRequest;
 
@@ -20,6 +21,19 @@ class RepliesController extends Controller
             ->setStatusCode(201);
     }
 	
+    public function userIndex(User $user)
+    {
+		app(\Dingo\Api\Transformer\Factory::class)->disableEagerLoading();
+		
+        $replies = $user->replies()->paginate(20);
+
+        if ($request->include) {
+            $replies->load(explode(',', $request->include));
+        }
+
+        return $this->response->paginator($replies, new ReplyTransformer());
+    }
+	
     public function destroy(Topic $topic, Reply $reply)
     {
         if ($reply->topic_id != $topic->id) {
@@ -30,5 +44,17 @@ class RepliesController extends Controller
         $reply->delete();
 
         return $this->response->noContent();
+    }
+	
+    public function index(Topic $topic)
+    {
+		app(\Dingo\Api\Transformer\Factory::class)->disableEagerLoading();
+        $replies = $topic->replies()->paginate(20);
+
+        if ($request->include) {
+            $replies->load(explode(',', $request->include));
+        }
+		
+        return $this->response->paginator($replies, new ReplyTransformer());
     }
 }
