fea68ee09172f1ce0cc41dc3717cdd8fcdb0d265
1 <1@qq.com>
2019-10-18 08:08:08 +0800

    topic update api

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
-
 $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api',
-    'middleware' => 'serializer:array'
+    'middleware' => ['serializer:array', 'bindings']
 ], function($api) {
 	
 	$api->group([
@@ -56,6 +54,8 @@ $api->version('v1', [
 			// 发布话题
 			$api->post('topics', 'TopicsController@store')
 				->name('api.topics.store');
+			$api->patch('topics/{topic}', 'TopicsController@update')
+				->name('api.topics.update');
         });
 	});
 
